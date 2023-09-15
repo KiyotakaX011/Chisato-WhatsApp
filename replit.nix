3c24c930879931be2625789b3aace769dbd97f3b
RA_ONE <125670877+7thRA-ONE@users.noreply.github.com>
2023-09-15 18:11:56 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,5 +1,12 @@
+
 { pkgs }: {
   deps = [
-    pkgs.sudo
+    pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+    pkgs.libuuid
+    pkgs.esbuild
   ];
+  env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];  }; 
 }
